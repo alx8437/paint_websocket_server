7adfb6508ba76cd5dac59fb045d8ea2eebca4190
Aleksandr Zelenskiy <dezion777@gmail.com>
2022-03-04 12:31:05 +0300

    ad ws port

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const WSserver = require('express-ws')(app);
 
 const PORT = process.env.PORT || 5000;
 
+app.ws('/', (ws, req) => {
+    console.log('connection was successful')
+})
+
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`)
 })
