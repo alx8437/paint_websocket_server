847fda6ed9dfff7bb46256feb1901497d9086e92
aleksandr_zelenskiy <dezion777@gmail.com>
2022-04-21 12:44:55 +0400

    added ws connectionHandler() and broadcastHandler()

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const app = express();
-const WSserver = require('express-ws')(app);
+const WSServer = require('express-ws')(app);
+const aWss = WSServer.getWss();
 
 const PORT = process.env.PORT || 5000;
 
 app.ws('/', (ws, req) => {
-    console.log('connection was successful')
+    console.log('Connection was successful')
+    ws.send('You connect successful')
+    ws.on('message', (msg => {
+        msg = JSON.parse(msg);
+        switch (msg.method) {
+            case 'connection':
+                connectionHandler(ws, msg)
+
+                break
+        }
+        console.log(JSON.parse(msg))
+    }))
+
 })
 
 app.listen(PORT, () => {
-    console.log(`server started on PORT ${PORT}`)
+    console.log(`Server started on PORT ${PORT}`)
 })
+
+const connectionHandler = (ws, msg) => {
+    ws.id = msg.id;
+    broadcastConnection(ws, msg);
+}
+
+const broadcastConnection = (ws, msg) => {
+    aWss.clients.forEach(client => {
+        if (client.id === msg.id) {
+            client.send(`User ${msg.username} was connected`)
+        }
+    })
+}
 
